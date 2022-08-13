1acab32a8a81f8d22d1cff7f383ca238fc1d3bf6
naiaddd <jaymacbarn@gmail.com>
2022-08-13 19:59:16 +1000

    2nd commit

--- main.py
+++ main.py
@@ -2,12 +2,20 @@
 import tcod
 from engine import Engine
 from entity import Entity
+from game_map import GameMap
+from procjen import generate_dungeon
 from input_handlers import EventHandler
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
 
+    map_width = 80
+    map_height = 45
+
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
     player_x = int(screen_width / 2)
     player_y = int(screen_height / 2)
@@ -24,8 +32,16 @@ def main() -> None:
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
     entities = {npc, player}
 
-    engine = Engine(entities=entities, event_handler=event_handler, player=player)
+    game_map = generate_dungeon(
+        max_rooms=max_rooms,
+        room_min_size=room_min_size,
+        room_max_size=room_max_size,
+        map_width=map_width,
+        map_height=map_height,
+        player=player
+    )
 
+    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
     with tcod.context.new_terminal(
         screen_width,
